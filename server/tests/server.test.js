8e7953c61a8541fc3a49c0c9d4117d543beeeccf
unknown <phamminhnhan1989@gmail.com>
2017-10-26 21:27:57 +0700

    add get/todo/:id test case

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+    _id: new ObjectID(),
     text: 'First request from postman'
   }, {
+    _id: new ObjectID(),
     text: 'Second request from postman'
   }
 ];
@@ -41,7 +44,7 @@ describe('POST/ todos', () => {
       });
   });
 
-  it('should not creat todo with invalid body data', (done) => {
+  it('should not create todo with invalid body data', (done) => {
     request(app)
       .post('/todos')
       .send("")
@@ -69,4 +72,32 @@ describe('GET/ todos', () => {
       })
       .end(done);
   });
+});
+
+describe('GET/ todos/:id', () => {
+  it('should return todo doc', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    var _id = new ObjectID();
+    request(app)
+      .get(`/todos/${_id.toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 for none object ids', (done) => {
+    const _id = 123;
+    request(app)
+      .get(`/todos/${_id}`)
+      .expect(404)
+      .end(done);
+  })
 });
